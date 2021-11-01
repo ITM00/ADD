260761ea00dc812294a7f7cd48fa72a6cc351774
Ivan <68062058+ITM00@users.noreply.github.com>
2021-11-01 18:47:45 +1000

    Update Folder_control.py

--- Folder_control.py
+++ Folder_control.py
@@ -4,16 +4,16 @@ from datetime import date
 import shutil
 
 def folder_control():
-    path = "/home/pi/AuDD/5_Photos/"  # replace with real path to photo folder
+    path = "/home/pi/AuDD/5_Photos/"
     
     my_date = date.today().weekday() # weekday 0 - monday, 6 - sunday
     if my_date == 0 or my_date == 1:
-        days = 6
+        days = 6  # For not to take in account weekends
     else:
-        days = 3
+        days = 3  # For work days
     seconds = time.time() - (days * 24 * 60 * 60)  # calculate time that was days ago
     
-    ### remove all dir older then 2 work week days. Except Semples
+    ### remove all dir older then days work week days. Except Semples folder
     dir_list = os.listdir(path)
 
     if len(dir_list) > 0:
@@ -30,7 +30,7 @@ def folder_control():
     ####  Create a folder with today name (YYYY-MM-DD), except weekends
     try:
         if str(date.today()) not in os.listdir(path) and my_date < 5:
-            os.umask(0)
+            os.umask(0)  # create a folder with permission for everyone to change it
             os.mkdir(path+str(date.today()), mode=0o777)
     except:
         pass
